9aca9932561e2edf44273a8c25f16f419516ca22
matecom57 <matecom57@gmail.com>
2026-02-22 06:15:30 -0600

    Test comité

--- copila.sh
+++ copila.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-rm $1.pdf
+sphinx-build -M html source outputdir
 
-pdflatex $1".tex"
+./open.sh
 
-rm $1.aux
-rm $1.log
-rm $1.nav
-rm $1.out
-rm $1.snm
-rm $1.toc
-
-open $1.pdf
 
---
+++ open.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+open outputdir/html/index.html 
+
